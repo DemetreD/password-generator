82df85cc741b1ed66f462b39f68b622e7d8b1995
DemetreD <136166143+DemetreD@users.noreply.github.com>
2026-08-17 22:39:55 +0400

    Add generate password function

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 //prettier-ignore
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
+
+const passwordOne = document.getElementById("password-one");
+const passwordTwo = document.getElementById("password-two");
+
+const generatePassword = function () {
+  let password = "";
+  for (let i = 1; i < 16; i++) {
+    let password1 = Math.floor(Math.random() * characters.length);
+    password += characters[password1];
+  }
+
+  return password;
+};
+
+const loadPasswords = function () {
+  passwordOne.textContent = generatePassword();
+  passwordTwo.textContent = generatePassword();
+};
